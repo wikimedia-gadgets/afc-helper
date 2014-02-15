c50737691d4da9681f9c414293bcc5738ec2e6c2
theopolisme <theopolismewiki@gmail.com>
2014-02-15 00:42:12 -0600

    Set up a bit of versioning and add details about testwiki

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 afch-rewrite [![Build Status](https://travis-ci.org/WPAFC/afch-rewrite.png)](https://travis-ci.org/WPAFC/afch-rewrite)
 ============
 
+v0.1 / "Exploding Fireball"
+
 A tool for reviewing Articles for Creation submissions on the English Wikipedia, rewritten using clear, object-oriented JavaScript with a focus on killing bloat while adding value.
 
 Currently mainly being developed by @theopolisme; not at all ready for prime time.
+
+[MediaWiki:Gadget-afch.js](https://test.wikipedia.org/wiki/MediaWiki:Gadget-afch.js)@testwiki is theoretically a working copy of the rewrite script, although it is also used for development purposes and may therefore be unstable at times. (`?debug=true` is your friend!)
--- src/afch.js
+++ src/afch.js
@@ -1,5 +1,4 @@
 //<nowiki>
-// Script should be located at [[MediaWiki:Gadget-afchelper.js]]
 ( function ( $, mw ) {
 	var pageName = mw.config.get( 'wgPageName' ), type;
 
@@ -19,12 +18,22 @@
 
 		// Set up constants
 		AFCH.consts = {};
+
+		// Master version data
+		AFCH.consts.version = 0.1;
+		AFCH.consts.versionName = 'Exploding Fireball';
+
+		// FIXME: Change when moving into production
+		AFCH.consts.beta = true;
+
 		AFCH.consts.scriptpath = mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' );
 		AFCH.consts.baseurl = AFCH.consts.scriptpath +
-			'?action=raw&ctype=text/javascript&title=MediaWiki:Gadget-afchelper.js';
+			'?action=raw&ctype=text/javascript&title=MediaWiki:Gadget-afch.js';
 
 		// FIXME: Right now mw.loader.using doesn't let you load urls :(
-		$.getScript( AFCH.consts.baseurl + '/core.js', function () {
+		$.getScript( AFCH.consts.baseurl + '/core.js' ).done( function () {
+			AFCH.log( 'core.js loaded' );
+
 			if ( AFCH.beforeLoad() ) {
 				AFCH.load( type );
 			} else {
