f017688e0649030956ac5ba28197ce0fb687b4b5
lorwp <lorwp@icloud.com>
2015-01-09 15:08:16 +1100

    Fix "OSError" Capitalisation.

--- scripts/upload.py
+++ scripts/upload.py
@@ -61,8 +61,8 @@ try:
 		sys.exit(1)
 	else:
 		print "Build succeeded!"
-except OsError:
-	print "OsError encountered. Attempting to use os.system..."
+except OSError:
+	print "OSError encountered. Attempting to use os.system..."
 	os.system(command)
 
 print 'Uploading to {}...'.format(wiki)
