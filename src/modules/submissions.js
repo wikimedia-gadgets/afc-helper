fd7d8405dadf17875e332b3e1065c2cfc96d8b50
Satyam Singh <56315878+satcasm@users.noreply.github.com>
2021-02-15 15:15:24 +0530

    remove draft article templates (#141)
    * updated Gruntfile.js
    
    * CA config
    
    * remove draft article templates
    
    * updated the removal Draft article templates
    
    * Update afch.js
    
    * Update afch.js
    
    * re-add newline, see what that does
    
    * remove cert files; addition was good but #139 will fix this soon
    
    * restore accidentally-removed file

--- src/modules/submissions.js
+++ src/modules/submissions.js
@@ -499,6 +499,8 @@
 			];
 
 		if ( isAccept ) {
+			// Remove Draft article templates
+			text = text.replace( /\{\{Draft(?!\|\s*text\s*=)(?: article(?!\|\s*text\s*=)(?:\|(?:subject=)?[^\|]+)?|\|(?:subject=)?[^\|]+)?\}\}/gi, '' );
 			// Uncomment cats and templates
 			text = text.replace( /\[\[:Category:/gi, '[[Category:' );
 			text = text.replace( /\{\{(tl|tlx|tlg)\|(.*?)\}\}/ig, '{{$2}}' );
