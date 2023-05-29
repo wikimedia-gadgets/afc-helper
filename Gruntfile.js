7618c67484bc9fdd03751b413a6ef54be17772d0
NovemLinguae <novemlinguae@gmail.com>
2023-05-29 10:21:17 -0700

    rename from teststyle to lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-exec' );
 
 	grunt.registerTask(
-		'teststyle',
+		'lint',
 		'Tests files for code style and code quality.',
 		[ 'eslint' ]
 	);
