4d57007411cd13d9a80cf9b61ff4594bae71c46a
zanhecht <797985+zanhecht@users.noreply.github.com>
2023-11-07 14:13:10 -0500

    Don't add recentPage to watchlist

--- src/modules/submissions.js
+++ src/modules/submissions.js
@@ -2364,7 +2364,8 @@
 
 						recentPage.edit( {
 							contents: newRecentText,
-							summary: 'Adding [[' + newPage + ']] to list of recent AfC creations'
+							summary: 'Adding [[' + newPage + ']] to list of recent AfC creations',
+							watchlist: 'nochange'
 						} );
 					} );
 
