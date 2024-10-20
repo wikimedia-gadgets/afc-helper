81cb3a6fbc47cd06216011f932632e96b05ec6e9
NovemLinguae <novemlinguae@gmail.com>
2024-10-19 17:09:14 -0700

    scaffold: refactor

--- tests/scaffold.js
+++ tests/scaffold.js
@@ -6,12 +6,39 @@
 
 /* eslint-env jest, node */
 
+const resetToBase = function () {
+	const fileSystem = require( 'fs' );
+	const basePageHtml = fileSystem.readFileSync( './tests/test-frame.html' ).toString();
+	// Set the base document content using jsdom
+	document.documentElement.innerHtml = basePageHtml;
+	AFCH = undefined;
+	$ = require( 'jquery' );
+	jQuery = $;
+};
+
+const setPageTitle = function ( title ) {
+	mw.config.get.mockImplementation( ( requested ) => {
+		if ( requested === 'wgPageName' ) {
+			return title;
+		} else if ( requested === 'wgNamespaceNumber' ) {
+			if ( title.indexOf( 'Draft:' ) === 0 ) {
+				return 118;
+			}
+		}
+	} );
+};
+
+resetToAFCApplicablePage = function () {
+	resetToBase();
+	setPageTitle( 'Draft:Foo' );
+	require( './../src/afch.js' );
+};
+
 jest.autoMockOff();
 
-const fs = require( 'fs' );
-
 // Mocked later
-mediaWiki = mw = {};
+mw = {};
+mediaWiki = {};
 
 // We're always mocking this, what the heck
 mw.config = {
@@ -32,31 +59,4 @@ mw.loader = {
 	}
 };
 
-const basePageHtml = fs.readFileSync( './tests/test-frame.html' ).toString();
-
-const setPageTitle = function ( title ) {
-	mw.config.get.mockImplementation( ( requested ) => {
-		if ( requested === 'wgPageName' ) {
-			return title;
-		} else if ( requested === 'wgNamespaceNumber' ) {
-			if ( title.indexOf( 'Draft:' ) === 0 ) {
-				return 118;
-			}
-		}
-	} );
-};
-
-const resetToBase = function () {
-	// Set the base document content using jsdom
-	document.documentElement.innerHtml = basePageHtml;
-	AFCH = undefined;
-	jQuery = $ = require( 'jquery' );
-};
-
 resetToBase();
-
-resetToAFCApplicablePage = function () {
-	resetToBase();
-	setPageTitle( 'Draft:Foo' );
-	require( './../src/afch.js' );
-};
