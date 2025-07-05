816b8bfac12ce91207bddda4be440625e163b8ba
Siddharth VP <siddharthvp@gmail.com>
2025-07-05 18:14:06 +0530

    Remove css minification
    Remove grunt-cssmin dependency. CSS minification is unnecessary in the build as it anyway gets handled by ResourceLoader. This also makes the on-wiki CSS file readable.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,29 +24,13 @@ module.exports = function ( grunt ) {
 		clean: {
 			build: {
 				src: [ 'build' ]
-			},
-			styling: {
-				src: [ 'build/less' ]
 			}
 		},
 
 		less: {
-			options: {
-				cleancss: true
-			},
-			files: {
-				expand: true,
-				cwd: 'src',
-				src: [ 'less/*.less' ],
-				dest: 'build',
-				ext: '.css'
-			}
-		},
-
-		cssmin: {
 			build: {
 				files: {
-					'build/afch.css': [ 'build/**/*.css' ]
+					'build/afch.css': [ 'src/less/general.less', 'src/less/submissions.less' ]
 				}
 			}
 		}
@@ -56,19 +40,12 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-less' );
-
-	grunt.registerTask(
-		'styling',
-		'Compiles LESS files to CSS and minifies them into one file.',
-		[ 'less', 'cssmin', 'clean:styling' ]
-	);
 
 	grunt.registerTask(
 		'build',
 		'Moves files to the /build directory, and minifies CSS.',
-		[ 'clean:build', 'copy', 'concat:dependencies', 'styling' ]
+		[ 'clean:build', 'copy', 'concat:dependencies', 'less' ]
 	);
 
 	grunt.registerTask( 'default', [ 'build' ] );
