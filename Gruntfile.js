92d6113cf9aad0aeb4e1a49e59f078c0225744e2
Siddharth VP <siddharthvp@gmail.com>
2025-07-07 04:07:10 +0530

    Remove minification and vendor-prefixing while building LESS files (#431)
    * Remove grunt-autoprefixer
    
    The only CSS rule requiring vendor-prefixing is user-select: none. It's supported everywhere except for Safari which needs the -webkit- prefix. Added it manually and removed the grunt-autoprefixer dependency. All other CSS being used in the gadget is standard.
    
    * Remove css minification
    
    Remove grunt-cssmin dependency. CSS minification is unnecessary in the build as it anyway gets handled by ResourceLoader. This also makes the on-wiki CSS file readable.
    
    * Remove minimal.less: empty and unused

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,61 +24,28 @@ module.exports = function ( grunt ) {
 		clean: {
 			build: {
 				src: [ 'build' ]
-			},
-			styling: {
-				src: [ 'build/less' ]
 			}
 		},
 
 		less: {
-			options: {
-				cleancss: true
-			},
-			files: {
-				expand: true,
-				cwd: 'src',
-				src: [ 'less/*.less' ],
-				dest: 'build',
-				ext: '.css'
-			}
-		},
-
-		autoprefixer: {
-			build: {
-				expand: true,
-				cwd: 'build',
-				src: [ '**/*.css' ],
-				dest: 'build'
-			}
-		},
-
-		cssmin: {
 			build: {
 				files: {
-					'build/afch.css': [ 'build/**/*.css' ]
+					'build/afch.css': [ 'src/less/general.less', 'src/less/submissions.less' ]
 				}
 			}
 		}
 
 	} );
 
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-less' );
-
-	grunt.registerTask(
-		'styling',
-		'Compiles LESS files to CSS and minifies them into one file.',
-		[ 'less', 'autoprefixer', 'cssmin', 'clean:styling' ]
-	);
 
 	grunt.registerTask(
 		'build',
 		'Moves files to the /build directory, and minifies CSS.',
-		[ 'clean:build', 'copy', 'concat:dependencies', 'styling' ]
+		[ 'clean:build', 'copy', 'concat:dependencies', 'less' ]
 	);
 
 	grunt.registerTask( 'default', [ 'build' ] );
