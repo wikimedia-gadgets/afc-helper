24162a62cd9c0f581d4b65907d1497775039ecbc
NovemLinguae <novemlinguae@gmail.com>
2025-12-12 12:30:30 -0800

    "Clean submission" should not delete {{AfC submission/draft}}
    fixes #447

--- tests/scaffold.js
+++ tests/scaffold.js
@@ -24,6 +24,9 @@ const setPageTitle = function ( title ) {
 			if ( title.indexOf( 'Draft:' ) === 0 ) {
 				return 118;
 			}
+		} else if ( requested === 'wgMonthNames' ) {
+			return [ '', 'January', 'February', 'March', 'April', 'May', 'June',
+				'July', 'August', 'September', 'October', 'November', 'December' ];
 		}
 	} );
 };
@@ -47,6 +50,14 @@ mw.config = {
 	get: jest.fn()
 };
 
+mw.config.get.mockImplementation( ( requested ) => {
+	if ( requested === 'wgMonthNames' ) {
+		return [ '', 'January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December' ];
+	}
+	return undefined;
+} );
+
 mw.user = {
 	getName: jest.fn()
 };
@@ -60,5 +71,41 @@ mw.loader = {
 		};
 	}
 };
+
+// Minimal mw.Title mock used by core.Page in unit tests
+mw.Title = function ( name ) {
+	this.name = name || '';
+};
+
+mw.Title.prototype.getPrefixedText = function () {
+	return this.name;
+};
+
+mw.Title.prototype.getMainText = function () {
+	const parts = this.name.split( ':' );
+	return parts.length > 1 ? parts.slice( 1 ).join( ':' ) : this.name;
+};
+
+mw.Title.prototype.getNamespaceId = function () {
+	if ( this.name.indexOf( 'User:' ) === 0 ) {
+		return 2;
+	}
+	if ( this.name.indexOf( 'Draft:' ) === 0 ) {
+		return 118;
+	}
+	// fall back to configured namespace number or 0
+	try {
+		return mw.config.get( 'wgNamespaceNumber' ) || 0;
+	} catch ( e ) {
+		return 0;
+	}
+};
+
+// Mirror mocks onto `mediaWiki` so modules that receive `mediaWiki` as
+// their `mw` parameter see the same mocks.
+mediaWiki.Title = mw.Title;
+mediaWiki.config = mw.config;
+mediaWiki.user = mw.user;
+mediaWiki.loader = mw.loader;
 
 resetToBase();
