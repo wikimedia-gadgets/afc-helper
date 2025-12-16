5367c392f648a079e6248a0a13c159cdc51f8e06
NovemLinguae <novemlinguae@gmail.com>
2025-12-15 18:23:44 -0800

    fix

--- jest.setup.js
+++ jest.setup.js
@@ -20,6 +20,9 @@ mw.config.get.mockImplementation( ( requested ) => {
 		return 'Draft:Foo';
 	} else if ( requested === 'wgNamespaceNumber' ) {
 		return 118;
+	} else if ( requested === 'wgMonthNames' ) {
+		return [ '', 'January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December' ];
 	}
 } );
 
@@ -29,9 +32,46 @@ mw.loader = {
 	}
 };
 
+
+// Minimal mw.Title mock used by core.Page in unit tests
+mw.Title = function ( name ) {
+	this.name = name || '';
+};
+
+mw.Title.prototype.getPrefixedText = function () {
+	return this.name;
+};
+
+mw.Title.prototype.getMainText = function () {
+	const parts = this.name.split( ':' );
+	return parts.length > 1 ? parts.slice( 1 ).join( ':' ) : this.name;
+};
+
+mw.Title.prototype.getNamespaceId = function () {
+	if ( this.name.indexOf( 'User:' ) === 0 ) {
+		return 2;
+	}
+	if ( this.name.indexOf( 'Draft:' ) === 0 ) {
+		return 118;
+	}
+	// fall back to configured namespace number or 0
+	try {
+		return mw.config.get( 'wgNamespaceNumber' ) || 0;
+	} catch ( e ) {
+		return 0;
+	}
+};
+
 mw.user = {
 	getName: jest.fn()
 };
+
+// Mirror mocks onto `mediaWiki` so modules that receive `mediaWiki` as
+// their `mw` parameter see the same mocks.
+mediaWiki.Title = mw.Title;
+mediaWiki.config = mw.config;
+mediaWiki.user = mw.user;
+mediaWiki.loader = mw.loader;
 
 $ = require( 'jquery' );
 jQuery = $;
