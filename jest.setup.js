6c67731b92ba1224a9c482e100ece6ae680f92d7
NovemLinguae <novemlinguae@gmail.com>
2025-12-21 05:38:33 -0800

    increase mock-mediawiki version, remove unneeded jest.setup.js code

--- jest.setup.js
+++ jest.setup.js
@@ -10,23 +10,6 @@ inUnitTestEnvironment = true;
 
 window.AFCH = {};
 
-$ = require( 'jquery' );
-jQuery = $;
-
-const _originalMwConfigGet = mw.config.get;
-mw.config.get = jest.fn( ( key ) => {
-	if ( key === 'wgMonthNames' ) {
-		return [ '', 'January', 'February', 'March', 'April', 'May', 'June',
-			'July', 'August', 'September', 'October', 'November', 'December' ];
-	}
-
-	if ( typeof _originalMwConfigGet === 'function' ) {
-		return _originalMwConfigGet.call( mw.config, key );
-	}
-
-	return undefined;
-} );
-
 require( './src/afch.js' );
 require( './src/modules/core.js' );
 require( './src/modules/submissions.js' );
